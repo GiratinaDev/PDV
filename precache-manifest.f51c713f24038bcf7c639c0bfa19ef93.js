7e68e0da4ddb074f39948c4d0584d9d799931256
GiratinaDev <krahil2013@gmail.com>
2020-08-09 18:10:01 +0100

    Updates

---
+++ precache-manifest.f51c713f24038bcf7c639c0bfa19ef93.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8570673e052645b95a81725a7318f54e",
+    "url": "/pdv/index.html"
+  },
+  {
+    "revision": "f24ff5ca1b7c9d10f83f",
+    "url": "/pdv/static/css/2.aa150c63.chunk.css"
+  },
+  {
+    "revision": "7b43eb5c023c00b62782",
+    "url": "/pdv/static/css/main.60284624.chunk.css"
+  },
+  {
+    "revision": "f24ff5ca1b7c9d10f83f",
+    "url": "/pdv/static/js/2.1fe93ea1.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/pdv/static/js/2.1fe93ea1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b43eb5c023c00b62782",
+    "url": "/pdv/static/js/main.897a2673.chunk.js"
+  },
+  {
+    "revision": "6f5e29acc36b114ec78c",
+    "url": "/pdv/static/js/runtime-main.dec04862.js"
+  },
+  {
+    "revision": "eb70466b7394d36012099c20290cd9f2",
+    "url": "/pdv/static/media/@fimage0.png.eb70466b.jpg"
+  },
+  {
+    "revision": "613464192e59aed91d550758f66a25af",
+    "url": "/pdv/static/media/Bin@freact.61346419.jpg"
+  },
+  {
+    "revision": "eb70466b7394d36012099c20290cd9f2",
+    "url": "/pdv/static/media/May@fimage0.png.eb70466b.jpg"
+  },
+  {
+    "revision": "57c40d7318a99df6de201588cee7d535",
+    "url": "/pdv/static/media/Na@fP1010416.57c40d73.JPG"
+  },
+  {
+    "revision": "38b51a04a4c0533906d01d2f56d6db47",
+    "url": "/pdv/static/media/Nama@ftest.38b51a04.jpg"
+  },
+  {
+    "revision": "613464192e59aed91d550758f66a25af",
+    "url": "/pdv/static/media/Name@freact.61346419.jpg"
+  },
+  {
+    "revision": "5766fddf48945cf7edf1f636d40a54a8",
+    "url": "/pdv/static/media/Name@fredux-react@.5766fddf.png"
+  },
+  {
+    "revision": "1d3aeac1979186fddccac35812059597",
+    "url": "/pdv/static/media/Nameste@fScreen Shot 2020-07-15.1d3aeac1.png"
+  },
+  {
+    "revision": "38b51a04a4c0533906d01d2f56d6db47",
+    "url": "/pdv/static/media/New@ftest.38b51a04.jpg"
+  },
+  {
+    "revision": "5766fddf48945cf7edf1f636d40a54a8",
+    "url": "/pdv/static/media/NewName@fredux-react@.5766fddf.png"
+  },
+  {
+    "revision": "38b51a04a4c0533906d01d2f56d6db47",
+    "url": "/pdv/static/media/Nm@ftest.38b51a04.jpg"
+  },
+  {
+    "revision": "eb70466b7394d36012099c20290cd9f2",
+    "url": "/pdv/static/media/Nme@fimage0.png.eb70466b.jpg"
+  },
+  {
+    "revision": "613464192e59aed91d550758f66a25af",
+    "url": "/pdv/static/media/Now@freact.61346419.jpg"
+  },
+  {
+    "revision": "eb70466b7394d36012099c20290cd9f2",
+    "url": "/pdv/static/media/Plz@fimage0.png.eb70466b.jpg"
+  },
+  {
+    "revision": "613464192e59aed91d550758f66a25af",
+    "url": "/pdv/static/media/Plz@freact.61346419.jpg"
+  },
+  {
+    "revision": "eb70466b7394d36012099c20290cd9f2",
+    "url": "/pdv/static/media/Title@fimage0.png.eb70466b.jpg"
+  },
+  {
+    "revision": "5766fddf48945cf7edf1f636d40a54a8",
+    "url": "/pdv/static/media/Title@fredux-react@.5766fddf.png"
+  },
+  {
+    "revision": "38b51a04a4c0533906d01d2f56d6db47",
+    "url": "/pdv/static/media/Title@ftest.38b51a04.jpg"
+  },
+  {
+    "revision": "eb70466b7394d36012099c20290cd9f2",
+    "url": "/pdv/static/media/Work@fimage0.png.eb70466b.jpg"
+  },
+  {
+    "revision": "38b51a04a4c0533906d01d2f56d6db47",
+    "url": "/pdv/static/media/bench in park@ftest.38b51a04.jpg"
+  },
+  {
+    "revision": "5766fddf48945cf7edf1f636d40a54a8",
+    "url": "/pdv/static/media/naem@fredux-react@.5766fddf.png"
+  },
+  {
+    "revision": "5766fddf48945cf7edf1f636d40a54a8",
+    "url": "/pdv/static/media/nama@fredux-react@.5766fddf.png"
+  },
+  {
+    "revision": "38b51a04a4c0533906d01d2f56d6db47",
+    "url": "/pdv/static/media/name@ftest.38b51a04.jpg"
+  },
+  {
+    "revision": "5766fddf48945cf7edf1f636d40a54a8",
+    "url": "/pdv/static/media/namw@fredux-react@.5766fddf.png"
+  },
+  {
+    "revision": "38b51a04a4c0533906d01d2f56d6db47",
+    "url": "/pdv/static/media/pak@ftest.38b51a04.jpg"
+  },
+  {
+    "revision": "613464192e59aed91d550758f66a25af",
+    "url": "/pdv/static/media/react.61346419.jpg"
+  },
+  {
+    "revision": "38b51a04a4c0533906d01d2f56d6db47",
+    "url": "/pdv/static/media/test.38b51a04.jpg"
+  }
+]);
